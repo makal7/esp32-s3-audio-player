09a806fc346174536b68cce48df0cde43483d0e6
makal7 <kalinec.martin@outlook.sk>
2024-04-26 22:01:15 +0200

    ui update

--- src/displayTools.cpp
+++ src/displayTools.cpp
@@ -1,16 +1,35 @@
 #include "displayTools.hpp"
 
 void DisplayTools::drawDisplayMenu(std::string title, std::vector<File_> list, int selected, U8G2_SH1106_128X64_NONAME_F_HW_I2C &display){
-    int offset = 0;
-    if (selected >= 3){
-        offset = list.size() - (list.size() - selected);
+    int pages = 0;
+    int pageA = 0;
+    int pageSize = 0;
+
+    if (list.size() > 0){
+        pages = (list.size()-1) / 4;
+    }else{
+        pages = 0;
     }
+    
+    pageA = selected / 4;
+
+    if (list.size() >= 4){
+        pageSize = 4;
+        if((list.size() - 4 * pageA) < 4){
+            pageSize = list.size() - 4 * pageA;
+        }
+    }else{
+        pageSize = list.size();
+    }
+
+    std::string pg = std::to_string(pageA + 1) + "/" + std::to_string(pages + 1);
 
     display.clearBuffer();
     display.setFont(FONT_BIG);
     display.drawUTF8(0,15,title.c_str());
     display.setFont(FONT_SMALL);
-    for(int i = 0 + offset, y = 0; i < list.size(); i++, y+=11){
+    display.drawUTF8(105,10,pg.c_str());
+    for(int i = 4 * pageA, y = 0; i < 4 * pageA + pageSize; i++, y+=10){
         display.drawUTF8(2, 15+11+4+y, list.at(i).name.c_str());
         if(i == selected)	
             display.drawFrame(0, 15+4+y+1, 128, 12);
